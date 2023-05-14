8a3ef808cfcac5f30b2473827315e23e0423cd6b
LAPTOP-QVD3M90T\niepo <242468@edu.p.lodz.pl>
2023-05-14 18:37:24 +0200

    etap2

--- Wspolbierzne/Dane/Kulka.cs
+++ Wspolbierzne/Dane/Kulka.cs
@@ -69,6 +69,7 @@ namespace Dane
 
         public async void move()
         {
+            //rusza asynchronicznie kulki
             await Task.Run(() => Rusz());
         }
         public void Rusz()
